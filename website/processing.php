2882fd7f25d459e8bd4d98fc524934eb5ceff6d2
JackQW <JackQW@7e42b2b0-f8d6-4373-867e-94bfd8f930d4>
2013-04-24 01:35:40 +0000

    added the .mwb.bak to ignore list
    changed zip from numeric(9) to char(10); don't care to deal with the complications associated w/ numeric storage
    
    further work on processing
    
    
    git-svn-id: https://xp-dev.com/svn/rvp/trunk@11 7e42b2b0-f8d6-4373-867e-94bfd8f930d4

--- website/processing.php
+++ website/processing.php
@@ -9,6 +9,7 @@ require_once("includes/validator.include.php");
 $valid = true;
 for ( array(
 		Validator::getValidator("UserName", "username" ),
+		Validator::getValidator("Password", "password" ),
 		Validator::getValidator("FirstName", "lastname" ),
 		Validator::getValidator("LastName", "lastname" ),
 		Validator::getValidator("City", "city" ),
@@ -19,7 +20,7 @@ for ( array(
 				'state' => $_REQUEST['state'],
 				'zipcode' => $_REQUEST['zip'],
 			) ),
-		) as $validator ) {
+		) as $field, $validator ) {
 	if ( $validator->valid() !== true )
 		$valid = false;
 }
@@ -31,6 +32,29 @@ $_SESSION['server_status'] = true;
 
 if ( $valid ) {
 	// TODO: sql registration
+	$db = @new mysqli( 'p:localhost', MYSQL_USER, MYSQL_PASS, 'proj4rv');
+	if ( $db->connect_errno !== 0 ) {
+		// TODO: handle connect failure
+	}
+	if (!($stmt = $db->prepare('INSERT INTO `proj4rv`.`user` (`username`, `password`, `email`, `first_name`, `last_name`, `city`, `state`, `zip`) VALUES (?, ?, ?, ?, ?, ?, ?, ?);')) {
+		// TODO: handle prepare failure
+	}
+	$stmt->bind_param("ssssssss",
+		$_REQUEST['username'],
+		md5($_REQUEST['password'], true),
+		$_REQUEST['email'],
+		$_REQUEST['first_name'],
+		$_REQUEST['last_name'],
+		$_REQUEST['city'],
+		$_REQUEST['state'],
+		$_REQUEST['zip'], // CHAR(10)
+	);
+	$stmt->bind_param("s", $_REQUEST['password']);
+	$stmt->bind_param("s", $_REQUEST['username']);
+	$stmt->bind_param("s", $_REQUEST['username']);
+	$stmt->bind_param("s", $_REQUEST['username']);
+
+
 }
 
 $_SESSION['processing_time'] = $processing_time;
